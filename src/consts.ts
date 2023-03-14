162b8c735d530ccfa2c46253d451a1773772d41a
Brunel <Jonathan.Brunel@amersports.com>
2023-03-14 16:19:46 +0100

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Pepite Finder';
+export const SITE_DESCRIPTION = 'Résumé des annonces de cession de biens';
---
+++ src/content/sample.md
@@ -0,0 +1,8 @@
+---
+title: "sample"
+description: ""
+pubDate: "Jul 08 2022"
+heroImage: "/placeholder-hero.jpg"
+draft: true
+---
+
